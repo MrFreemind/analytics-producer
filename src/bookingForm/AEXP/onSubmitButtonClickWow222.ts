61c2420b59ca7f1519446c8587d979c119409cb4
Kotelnikov Dmitry <kotelnikoff.dmitry@gmail.com>
2023-11-29 02:56:03 +0300

    Im testing smth

--- src/bookingForm/AEXP/onSubmitButtonClickWow222.ts
+++
@@ -1,8 +0,0 @@
-import createAnalyticsFunction from '@/createAnalyticsFunction'
-
-export type Data = {
-    source: 'bookingForm' | 'bookingFormModal' | 'bookingFormModalButton';
-}
-//
-
-export default createAnalyticsFunction<Data>('onSubmit/button/clickWOW')
