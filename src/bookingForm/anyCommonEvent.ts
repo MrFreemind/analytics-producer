57c54ecf9bc25d2a2ed5f4a25f3871c2b39139d6
Kotelnikov Dmitry <kotelnikoff.dmitry@gmail.com>
2023-11-29 13:29:59 +0300

    2

--- src/bookingForm/anyCommonEvent.ts
+++ src/bookingForm/anyCommonEvent.ts
@@ -1,9 +1,10 @@
 import createAnalyticsFunction from '@/createAnalyticsFunction'
 
 export type Data = {
-  uuid: string;
-  name: string;
+  uuid: string; // PassengerUuid
+  name: string; // Passenger full name
   id: number;
 }
 
+// Trigger on any event
 export default createAnalyticsFunction<Data>('any/common/event')
