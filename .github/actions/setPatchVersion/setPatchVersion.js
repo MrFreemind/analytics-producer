82c6ccaae498a85b903e85e25349ff1c622f7a3a
Dmitry Kotelnikov <kotelnikov.dmitry@pm.me>
2023-11-29 03:17:25 +0300

    Trying new

--- .github/actions/setPatchVersion/setPatchVersion.js
+++ .github/actions/setPatchVersion/setPatchVersion.js
@@ -3,8 +3,6 @@ const github = require('@actions/github');
 const path = require('path');
 const { existsSync, readFileSync, writeFileSync } = require('fs');
 
-
-
 const workspace = process.env.GITHUB_WORKSPACE || path.dirname(__dirname);
 console.log('process.env.GITHUB_WORKSPACE', process.env.GITHUB_WORKSPACE);
 console.log('workspace', workspace);
@@ -17,31 +15,20 @@ function getPackageJson() {
 
 try {
 
-  const nextPatchVersionFromAction = core.getInput('version');
-  const nextPatchVersion = 34;
+  const nextMinorVersion = core.getInput('version');
+  const nextPatchVersion = core.getInput('run_number')
 
-  console.log('run_attempt', core.getInput('run_attempt'))
-  console.log('run_id', core.getInput('run_id'))
-  console.log('run_number', core.getInput('run_number'))
-
-  console.log({ nextPatchVersionFromAction, nextPatchVersion })
+  console.log({ nextMinorVersion, nextPatchVersion })
 
   const pkg = getPackageJson();
 
-  const [major, minor] = pkg.version.split('.');
+  const [major] = pkg.version.split('.');
 
-  const nextVersion = `${major}.${minor}.${nextPatchVersion}`;
-
-  console.log('2', pkg.version)
+  const nextVersion = `${major}.${nextMinorVersion}.${nextPatchVersion}`;
 
   const file = readFileSync(path.join(workspace, 'package.json'), 'utf-8');
   const newFile = file.replace(pkg.version, nextVersion);
   writeFileSync(path.join(workspace, 'package.json'), newFile, 'utf-8');
-
-  core.setOutput("time", 'yes');
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
