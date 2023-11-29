be26dcd0e8303482cc6f7a78c852f15a5dd642e7
Dmitry Kotelnikov <kotelnikoff.dmitry@gmail.com>
2023-11-29 15:11:14 +0300

    Added new event for AEXP

---
+++ src/bookingForm/AEXP/onEnterFlightNumber.ts
@@ -0,0 +1,15 @@
+import createAnalyticsFunction from '@/createAnalyticsFunction'
+
+export type Data = {
+    flightNumber: string;
+    userId: number;
+    flowUuid: string;
+}
+
+const example: Data = {
+    flightNumber: 123,
+    userId: 123,
+    flowUuid: '123qwe-qwe'
+}
+
+export default createAnalyticsFunction<Data>('bookingForm|aexp|onEnterFlightNumber')
